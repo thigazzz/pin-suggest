a6fec686bb48d7436be5a83802c7a6cb5c20f776
thigazzz <thiago.p.dasilva2005@gmail.com>
2024-05-29 11:28:49 -0300

    feat: favorite and unfavorite commands + separate the app in groups of subcommands

--- cli.py
+++ cli.py
@@ -1,17 +1,16 @@
 from typing import Annotated, Optional
 import typer
 app = typer.Typer()
+images_commander = typer.Typer()
 
 from pinsuggest.gallery import Gallery
 
-# TODO: Found a way to not setup app
+# TODO: Found a way to keep the context between the commands
 def setup_app():
     global gallery
     global albuns
     gallery = Gallery()
     albuns =  gallery.get_albuns()
-    
-
 setup_app()
 
 @app.command()
@@ -23,8 +22,8 @@ def topics():
         c += 1
     print("\nDigite o comando 'images' + o número do tópico desejado: ")
 
-@app.command()
-def images(number_of_topic: int, number_of_images_to_get: Annotated[Optional[int], typer.Argument()] = None):
+@images_commander.command()
+def show(number_of_topic: int, number_of_images_to_get: Annotated[Optional[int], typer.Argument()] = None):
     current_album = albuns[number_of_topic - 1]
     if number_of_images_to_get:
         gallery.change_number_of_images(album=current_album, number_of_images_to_get=number_of_images_to_get)
@@ -35,6 +34,26 @@ def images(number_of_topic: int, number_of_images_to_get: Annotated[Optional[int
     for image in current_album.images:
         print(f'[{c}] {image.title}: {image.link_to}\n')
         c += 1
+@images_commander.command()
+def favorite(topic: int, image: int):
+    current_album = albuns[topic - 1]
+    images = current_album.get_images()
+    print(images[image - 1]._is_favorited)
+    current_album.favorite_image(images[image - 1])
+    print(images[image - 1]._is_favorited)
+
+@images_commander.command()
+def unfavorite(topic: int, image: int):
+    current_album = albuns[topic - 1]
+    images = current_album.get_images()
+    if images[image - 1]._is_favorited == False:
+        print("Imagem não está favoritada para desfavoritar")
+        return
+    print(images[image - 1]._is_favorited)
+    current_album.unfavorite_image(images[image - 1])
+    print(images[image - 1]._is_favorited)
+
+app.add_typer(images_commander, name='images')
 
 
 if __name__ == "__main__":
